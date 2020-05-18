e7bad92c970e9b6876756672c1b657527b801ab2
Andrey O <ozzy@triasoft.com>
2020-05-18 15:28:21 +0300

    Add Extension method Tab<>()

---
+++ src/MyLab.Db/DataContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LinqToDB;
+
+namespace MyLab.Db
+{
+    /// <summary>
+    /// Contains extensions for <see cref="LinqToDB.IDataContext"/>
+    /// </summary>
+    public static class DataContextExtensions
+    {
+        /// <summary>
+        /// Returns queryable source for specified mapping class for current connection, mapped to database table or view.
+        /// </summary>
+        public static ITable<T> Tab<T>(this IDataContext dataContext)
+            where T : class
+        {
+            if (dataContext == null) throw new ArgumentNullException(nameof(dataContext));
+
+            return dataContext.GetTable<T>();
+        }
+    }
+}
